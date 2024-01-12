7a20a4c41e5f2e641b0b97dd2116e3ae944163cc
VIPon4ik <umniyd@gmail.com>
2024-01-12 16:57:03 +0200

    Created button component

--- src/app/shared/ui/user-card/user-card.component.ts
+++ src/app/shared/ui/user-card/user-card.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../types/user';
 import { MatCardModule } from '@angular/material/card';
+import { ButtonComponent } from '../../../button/button.component';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, ButtonComponent],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
 export class UserCardComponent {
   @Input() user!: User;
+
+  onClick() {
+    console.log('click')
+  }
 }
