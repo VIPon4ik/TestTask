e609e7dc5cf659069b68d18a6e71517c4842d6c3
VIPon4ik <umniyd@gmail.com>
2024-01-12 23:35:53 +0200

    Styled links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './users/feature/user-list/user-list.component';
+import { SavedListComponent } from './saved/feature/saved-list/saved-list.component';
 
-export const routes: Routes = [{ path: '', component: UserListComponent}];
+export const routes: Routes = [{ path: '', component: UserListComponent},
+  { path: 'saved', component: SavedListComponent}
+];
